dd50524968e50885a125ef9568c050b5814735b7
yahavg08 <yahavgoldshtein1108@gmail.com>
2024-10-06 12:50:09 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+	public static int legalCities(CameraInfo[]cameras) {
+		int count=0;
+    boolean flag=true;
+		for (int i=0; i<100; i++) {
+			for (int j=0;j<cameras.length; j++) {
+				if (cameras[j].getCity()==i) {
+					if (cameras[j].allGood()==true) {
+						flag=true;
+					}
+					else {
+						flag=false;
+					}
+				}
+			}
+					if (flag==true) {
+						count++;
+					}
+		}
+		return count;
 }
